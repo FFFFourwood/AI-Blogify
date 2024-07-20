0514009198af27ca7c6e0c1878323d3250fe80a6
Arthur Li <34063465+FFFFourwood@users.noreply.github.com>
2024-07-20 20:27:19 +0800

    add breadcrumbs

--- aiblogify-web/src/app/articles/page.tsx
+++ aiblogify-web/src/app/articles/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 import React from "react";
-
+import { Breadcrumb } from "@/components/Breadcrumbs";
 import { Permission } from "@/utils/permissions";
 
 const Article: React.FC = () => {
-    return <h1 className="text-3xl font-bold">Article</h1>;
+    const breadcrumbs = [{ name: "Articles", href: "/articles" }];
+    return (
+        <div className="ai-blogify-content-wrap">
+            <div className="flex">
+                <Breadcrumb items={breadcrumbs} />
+            </div>
+        </div>
+    );
 };
 
 export default Article;
--- aiblogify-web/src/app/page.tsx
+++ aiblogify-web/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 const Home = () => (
-    <div className="App">
+    <div className="App ai-blogify-content-wrap">
         <div>
             <Image src="/vercel.svg" alt="Vercel Logo" className="dark:invert" width={100} height={24} priority />
         </div>
