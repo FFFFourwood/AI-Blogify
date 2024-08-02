a28b4920f3e529f763ccd8abc71d46f491c2f2e8
Arthur Li <34063465+FFFFourwood@users.noreply.github.com>
2024-08-02 14:56:35 +0800

    get articles by categoryid

--- aiblogify-server/src/controllers/articleController.ts
+++ aiblogify-server/src/controllers/articleController.ts
@@ -4,65 +4,72 @@ import logger from '../utils/logger';
 // get all articles
 export const getAllArticles = async (req: Request, res: Response): Promise<void> => {
     const { page = 1, limit = 10 } = req.query;
-    logger.info(`getAllArticles:Getting all articles`);
+    logger.info(`Article:Getting all articles`);
     try {
         const articles = await articleService.getAllArticles(Number(page), Number(limit));
-        logger.info(`getAllArticles:Articles fetched successfully`);
+        logger.info(`Article:Articles fetched successfully`);
         res.status(200).json(articles);
     } catch (error) {
-        logger.error(`getAllArticles:Error fetching articles`);
+        logger.error(`Article:Error fetching articles`);
         res.status(400).json({ message: 'Error fetching articles', error });
     }
 };
 
-
-
-
+// get Article by id
+export const getArticle = async (req: Request, res: Response) => {
+    logger.info(`Article:Getting article by id`);
+    try {
+        const article = await articleService.getArticleById(req.params.id);
+        if (!article) {
+            logger.error(`Article:Article not found`);
+            return res.status(404).json({ message: 'Article not found' });
+        }
+        logger.info(`Article:Article fetched successfully`);
+        res.json(article);
+    } catch (error) {
+        logger.error(`Article:Error fetching article`);
+        res.status(500).json({ message: 'Error fetching article', error });
+    }
+};
 
 
 // get articles by category
 export const getArticlesByCategory = async (req: Request, res: Response): Promise<void> => {
     const { categoryId } = req.params;
     const { page = 1, limit = 10 } = req.query;
-    logger.info(`getArticlesByCategory:Getting articles by category`);
+    logger.info(`Article:Getting articles by category`);
     try {
         const articles = await articleService.getArticlesByCategory(categoryId, Number(page), Number(limit));
-        logger.info(`getArticlesByCategory:Articles fetched successfully`);
+        logger.info(`Article:Articles fetched successfully`);
         res.status(200).json(articles);
     } catch (error) {
-        logger.error(`getArticlesByCategory:Error fetching articles by category`);
+        logger.error(`Article:Error fetching articles by category`);
         res.status(400).json({ message: 'Error fetching articles by category', error });
     }
 };
+
+
+
+
+
 
 //get articles by tag
 export const getArticlesByTag = async (req: Request, res: Response): Promise<void> => {
     const { tagId } = req.params;
     const { page = 1, limit = 10 } = req.query;
-    logger.info(`getArticlesByTag:Getting articles by tag`);
+    logger.info(`Article:Getting articles by tag`);
 
     try {
         const articles = await articleService.getArticlesByTag(tagId, Number(page), Number(limit));
-        logger.info(`getArticlesByTag:Articles fetched successfully`);
+        logger.info(`Article:Articles fetched successfully`);
         res.status(200).json(articles);
     } catch (error) {
-        logger.error(`getArticlesByTag:Error fetching articles by tag`);
+        logger.error(`Article:Error fetching articles by tag`);
         res.status(400).json({ message: 'Error fetching articles by tag', error });
     }
 };
 
-// get Article by id
-export const getArticle = async (req: Request, res: Response) => {
-    try {
-        const article = await articleService.getArticleById(req.params.id);
-        if (!article) {
-            return res.status(404).json({ message: 'Article not found' });
-        }
-        res.json(article);
-    } catch (error) {
-        res.status(500).json({ message: 'Error fetching article', error });
-    }
-};
+
 
 // 创建新文章
 export const createArticle = async (req: Request, res: Response) => {
--- aiblogify-server/src/services/articleService.ts
+++ aiblogify-server/src/services/articleService.ts
@@ -1,8 +1,9 @@
 import Article, { IArticle } from '../models/articleModel';
-import { FilterQuery } from 'mongoose';
+import { FilterQuery, Types } from 'mongoose';
 import { extractImageUrls } from '../utils/utils';
 import Image from '../models/imageModel';
 import { ObjectId } from 'bson';
+import logger from '../utils/logger';
 
 const aggregateArticles = async (filter: FilterQuery<IArticle>, page: number, limit: number): Promise<any> => {
     const articles = await Article.aggregate([
@@ -147,18 +148,162 @@ const aggregateArticles = async (filter: FilterQuery<IArticle>, page: number, li
 const getAllArticles = async (page: number, limit: number): Promise<any> => {
     return aggregateArticles({}, page, limit);
 };
-const getArticlesByCategory = async (categoryId: string, page: number, limit: number): Promise<any> => {
-    return aggregateArticles({ categories: new ObjectId(categoryId) }, page, limit);
-};
 
-const getArticlesByTag = async (tagId: string, page: number, limit: number): Promise<any> => {
-    return aggregateArticles({ tags: new ObjectId(tagId) }, page, limit);
-};
 
 const getArticleById = async (id: string): Promise<any | null> => {
     const articles = await aggregateArticles({ _id: new ObjectId(id) }, 1, 1);
     return articles.docs.length ? articles.docs[0] : null;
 };
+
+
+const getArticlesByCategory = async (categoryId: string, page: number, limit: number): Promise<any> => {
+    const categoryObjectId = new Types.ObjectId(categoryId);
+    logger.info(`ArticleService:Getting articles by category`);
+    const articles = await Article.aggregate([
+        {
+            $lookup: {
+                from: 'articlecategories',
+                localField: '_id',
+                foreignField: 'articleId',
+                as: 'articlecategories'
+            }
+        },
+        {
+            $unwind: {
+                path: '$articlecategories',
+                preserveNullAndEmptyArrays: false // 我们不需要保留空数组
+            }
+        },
+        {
+            $match: {
+                'articlecategories.categoryId': categoryObjectId
+            }
+        },
+        { $sort: { createdAt: -1 } },
+        { $skip: (page - 1) * limit },
+        { $limit: limit },
+        {
+            $lookup: {
+                from: 'images',
+                localField: '_id',
+                foreignField: 'article',
+                as: 'images'
+            }
+        },
+        {
+            $lookup: {
+                from: 'articletags',
+                localField: '_id',
+                foreignField: 'articleId',
+                as: 'articletags'
+            }
+        },
+        {
+            $unwind: {
+                path: '$articletags',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $lookup: {
+                from: 'categories',
+                localField: 'articlecategories.categoryId',
+                foreignField: '_id',
+                as: 'categories'
+            }
+        },
+        {
+            $lookup: {
+                from: 'tags',
+                localField: 'articletags.tagId',
+                foreignField: '_id',
+                as: 'tags'
+            }
+        },
+        {
+            $unwind: {
+                path: '$categories',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $unwind: {
+                path: '$tags',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group: {
+                _id: '$_id',
+                title: { $first: '$title' },
+                content: { $first: '$content' },
+                createdAt: { $first: '$createdAt' },
+                updatedAt: { $first: '$updatedAt' },
+                slug: { $first: '$slug' },
+                status: { $first: '$status' },
+                commentsCounts: { $first: '$commentsCounts' },
+                views: { $first: '$views' },
+                likes: { $first: '$likes' },
+                type: { $first: '$type' },
+                description: { $first: '$description' },
+                coverImg: { $first: '$coverImg' },
+                images: { $first: { $map: { input: "$images", as: "image", in: "$$image.url" } } },
+                categories: { $addToSet: '$categories' },
+                tags: { $addToSet: '$tags' },
+            }
+        },
+        {
+            $project: {
+                title: 1,
+                content: 1,
+                categories: 1,
+                tags: 1,
+                createdAt: 1,
+                updatedAt: 1,
+                slug: 1,
+                status: 1,
+                commentsCounts: 1,
+                views: 1,
+                likes: 1,
+                type: 1,
+                description: 1,
+                coverImg: 1,
+                images: 1,
+            }
+        },
+        {
+            $sort: { createdAt: -1 } // 再次按照创建时间进行排序
+        }
+    ]).exec();
+    logger.info(`ArticleService:Articles fetched successfully`);
+    const total = await Article.countDocuments({
+        _id: { $in: articles.map(article => article._id) }
+    });
+    return {
+        docs: articles,
+        totalDocs: total,
+        limit,
+        page,
+        totalPages: Math.ceil(total / limit),
+        pagingCounter: (page - 1) * limit + 1,
+        hasPrevPage: page > 1,
+        hasNextPage: page < Math.ceil(total / limit),
+        prevPage: page > 1 ? page - 1 : null,
+        nextPage: page < Math.ceil(total / limit) ? page + 1 : null
+    };
+};
+
+
+
+
+
+
+
+
+const getArticlesByTag = async (tagId: string, page: number, limit: number): Promise<any> => {
+    return aggregateArticles({ tags: new ObjectId(tagId) }, page, limit);
+};
+
 
 const createArticle = async (title: string, content: string, authorId: string): Promise<IArticle> => {
     const newArticle = await Article.create({
